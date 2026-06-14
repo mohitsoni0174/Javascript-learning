857e8514a25fa1e4dfaf41d0c067392eca158284
Mohit Soni <mohitsoni1704@gmail.com>
2026-06-14 08:02:02 +0530

    Add variable declaration examples

--- variables/index.js
+++ variables/index.js
@@ -3,3 +3,18 @@ let age = 23;
 
 console.log(name);
 console.log(age);
+
+// Variable Declaration in JavaScript
+
+let name = "Mohit";
+const age = 23;
+var city = "Lucknow";
+
+console.log(name);
+console.log(age);
+console.log(city);
+
+// Reassigning let
+name = "Soni";
+
+console.log(name);
